0b2f301a0cb23c7911374c742fac347596c21c25
KerimKochekov <kerim.kocekow@gmail.com>
2021-04-26 08:56:09 +0300

    Api docs added [GET only for now]

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,81 @@ var router = express.Router();
 var problem_controller = require("../controllers/problemController");
 var solution_controller = require("../controllers/solutionController");
 
-router.post("/problems", problem_controller.api_problem_create_post);
+/**
+ * @swagger
+ * /problem/create:
+ *   get:
+ *     summary: Create new problem
+ *     description: Create new problem
+ *     responses:
+ *       200:
+ *         description: Create Problem
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *      
+ *       404:
+ *         description: Create problem page Not Found
+ */
+router.get("/problem/create", problem_controller.api_problem_create_get);
 
-// GET request for one Problem.
-router.get("/problems/:id", problem_controller.api_problem_detail);
+// POST request for creating Solution.
+router.post("/problem/create", problem_controller.api_problem_create_post);
+
+/**
+ * @swagger
+ * /api/problem/{id}:
+ *   get:
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema: 
+ *          type: integer
+ *     summary: Retrieve a problem with specific id.
+ *     description: Retrieve a problem with specific id.
+ *     responses:
+ *       200:
+ *         description: A single problem with specific id.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   _id:
+ *                     type: string
+ *                     description: The problem ID.
+ *                     example: 607de21470ad2a2b0b56a125
+ *                   title:
+ *                     type: string
+ *                     description: The problem's title.
+ *                     example: hello world
+ *                   detail:
+ *                     type: string
+ *                     description: The problem's detail.
+ *                     example: print hello world
+ *                   solution:
+ *                     type: array
+ *                     items:
+ *                        type: object
+ *                        properties:
+ *                          status:
+ *                            type: string
+ *                            description: The solution status
+ *                            example: One of these verdicts ["submitted", "ok", "failed", "running"] 
+ *                          _id:
+ *                            type: string
+ *                            description: The solution ID.
+ *                            example: 607de21470ad2a2b0b56a125
+ *                          problem:
+ *                            type: object
+ *                            description: The belonging problem
+ *                            example:  6082d2b2f5391c900d9a2560
+ *       404:
+ *         description: Problem Not Found
+ */
+router.get("/problem/:id", problem_controller.api_problem_detail);
 
 /// PROBLEM ROUTES ///
 
@@ -40,16 +111,54 @@ router.get("/problems/:id", problem_controller.api_problem_detail);
  *                     type: string
  *                     description: The problem's detail.
  *                     example: print hello world
+ *      
+ *       404:
+ *         description: List of Problems Not Found
  */
 router.get("/problems", problem_controller.api_problem_list);
 
 /// SOLUTION ROUTES ///
 
-// POST request for creating Solution.
-router.post("/solutions", solution_controller.api_solution_create_post);
+router.get("/solution/create", solution_controller.api_solution_create_get);
+// POST request for creating Problem.
+router.post("/solution/create", solution_controller.api_solution_create_post);
 
-// GET request for one Solution.
-router.get("/solutions/:id", solution_controller.api_solution_detail);
+/**
+ * @swagger
+ * /api/solution/{id}:
+ *   get:
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema: 
+ *          type: integer
+ *     summary: Retrieve a solution with specific id.
+ *     description: Retrieve a solution with specific id.
+ *     responses:
+ *       200:
+ *         description: A single solution with specific id.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   status:
+ *                     type: string
+ *                     description: The solution status
+ *                     example: One of these verdicts ["submitted", "ok", "failed", "running"] 
+ *                   _id:
+ *                     type: string
+ *                     description: The solution ID.
+ *                     example: 607de21470ad2a2b0b56a125
+ *                   problem:
+ *                     type: object
+ *                     description: The belonging problem
+ *                     example:  { "_id": "6082d2b2f5391c900d9a2560", "title": "easy", "detail": "print hello world"}
+ *       404:
+ *         description: Solution Not Found
+ */
+router.get("/solution/:id", solution_controller.api_solution_detail);
 
 /**
  * @swagger
@@ -67,18 +176,20 @@ router.get("/solutions/:id", solution_controller.api_solution_detail);
  *               items:
  *                 type: object
  *                 properties:
+ *                   status:
+ *                     type: string
+ *                     description: The solution status
+ *                     example: One of these verdicts ["submitted", "ok", "failed", "running"] 
  *                   _id:
  *                     type: string
- *                     description: The problem ID.
+ *                     description: The solution ID.
  *                     example: 607de21470ad2a2b0b56a125
- *                   title:
- *                     type: string
- *                     description: The problem's title.
- *                     example: hello world
- *                   detail:
- *                     type: string
- *                     description: The problem's detail.
- *                     example: print hello world
+ *                   problem:
+ *                     type: object
+ *                     description: The belonging problem
+ *                     example:  { "_id": "6082d2b2f5391c900d9a2560", "title": "easy", "detail": "print hello world"}
+ *       404:
+ *         description: List of Solutions Not Found
  */
 router.get("/solutions", solution_controller.api_solution_list);
 
