5cfcf400d90531b2ae5e4b5a2b1100d741c5c5ee
KerimKochekov <kerim.kocekow@gmail.com>
2021-04-26 18:04:21 +0300

    Full API Docs added

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,31 +6,58 @@ var problem_controller = require("../controllers/problemController");
 var solution_controller = require("../controllers/solutionController");
 
 /**
- * @swagger
- * /problem/create:
- *   get:
- *     summary: Create new problem
- *     description: Create new problem
- *     responses:
- *       200:
- *         description: Create Problem
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *      
- *       404:
- *         description: Create problem page Not Found
- */
-router.get("/problem/create", problem_controller.api_problem_create_get);
+ * @swagger 
+ * tags:
+ *    name: Online-Judge
+ *    description: Online Judge for creating problems and judging solutions
+ * */
 
-// POST request for creating Solution.
+/**
+ * @swagger
+ * /api/problem/create:
+ *     post:
+ *       tags: [Online-Judge]
+ *       summary: Create a new Problem
+ *       description: Send JSON object to database
+ * 
+ *       parameters:
+ *        - in: Name of Problem
+ *          name: title
+ *          required: true
+ *        - in: Detail
+ *          name: detail
+ *          required: true
+ *       responses:
+ *         201:
+ *           description: The problem created successfully 
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     description: The problem ID.
+ *                     example: 607de21470ad2a2b0b56a125
+ *                   title:
+ *                     type: string
+ *                     description: The problem's title.
+ *                     example: hello world
+ *                   detail:
+ *                     type: string
+ *                     description: The problem's detail.
+ *                     example: print hello world
+ *      
+ *         500:
+ *           description: Some server error
+ */
 router.post("/problem/create", problem_controller.api_problem_create_post);
 
 /**
  * @swagger
- * /api/problem/{id}:
+ * /api/problems/{id}:
  *   get:
+ *     tags: [Online-Judge]
  *     parameters:
  *      - in: path
  *        name: id
@@ -79,7 +106,7 @@ router.post("/problem/create", problem_controller.api_problem_create_post);
  *       404:
  *         description: Problem Not Found
  */
-router.get("/problem/:id", problem_controller.api_problem_detail);
+router.get("/problems/:id", problem_controller.api_problem_detail);
 
 /// PROBLEM ROUTES ///
 
@@ -87,6 +114,7 @@ router.get("/problem/:id", problem_controller.api_problem_detail);
  * @swagger
  * /api/problems:
  *   get:
+ *     tags: [Online-Judge]
  *     summary: Retrieve a list of problems.
  *     description: Retrieve a list of problems.
  *     responses:
@@ -119,14 +147,53 @@ router.get("/problems", problem_controller.api_problem_list);
 
 /// SOLUTION ROUTES ///
 
-router.get("/solution/create", solution_controller.api_solution_create_get);
-// POST request for creating Problem.
+/**
+ * @swagger
+ * /api/solution/create:
+ *     post:
+ *       tags: [Online-Judge]
+ *       summary: Create a new Solution
+ *       description: Send JSON object to database
+ * 
+ *       parameters:
+ *        - in: Problem
+ *          name: problem-id
+ *          required: true
+ *        - in: Choose File
+ *          name: file
+ *          required: true
+ *          type: file
+ *       responses:
+ *         201:
+ *           description: The solution created successfully 
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   status:
+ *                     type: string
+ *                     description: The solution status
+ *                     example: One of these verdicts ["submitted", "ok", "failed", "running"] 
+ *                   _id:
+ *                     type: string
+ *                     description: The solution ID.
+ *                     example: 607de21470ad2a2b0b56a125
+ *                   problem:
+ *                     type: object
+ *                     description: The belonging problem
+ *                     example:  { "_id": "6082d2b2f5391c900d9a2560", "title": "easy", "detail": "print hello world"}
+ *      
+ *         500:
+ *           description: Some server error
+ */
 router.post("/solution/create", solution_controller.api_solution_create_post);
 
 /**
  * @swagger
- * /api/solution/{id}:
+ * /api/solutions/{id}:
  *   get:
+ *     tags: [Online-Judge]
  *     parameters:
  *      - in: path
  *        name: id
@@ -158,12 +225,13 @@ router.post("/solution/create", solution_controller.api_solution_create_post);
  *       404:
  *         description: Solution Not Found
  */
-router.get("/solution/:id", solution_controller.api_solution_detail);
+router.get("/solutions/:id", solution_controller.api_solution_detail);
 
 /**
  * @swagger
  * /api/solutions:
  *   get:
+ *     tags: [Online-Judge]
  *     summary: Retrieve a list of solutions.
  *     description: Retrieve a list of solutions.
  *     responses:
