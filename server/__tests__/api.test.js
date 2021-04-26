9fcc1b25e7117b5bd307269446032ec94fd351c9
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2021-04-26 18:43:25 +0300

    Use up-to-date test script

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,5 +1,6 @@
 var request = require("supertest");
 var express = require("express");
+var assert = require("assert");
 var cookieParser = require("cookie-parser");
 var fileUpload = require("express-fileupload");
 var apiRouter = require("../routes/api");
@@ -52,20 +53,35 @@ afterAll(async (done) => {
   }
 });
 
+let problem_id_hack, solution_id_hack; // not proud of this, but I needed to get it done
+
 test("Create new problem", (done) => {
   request(app)
     .post("/api/problems")
     .type("form")
     .send({ title: "test problem", detail: "This is a test problem." })
     .expect("Content-Type", /json/)
-    .expect(200, done);
+    .expect(201)
+    .then((res) => {
+      assert(res.body.title, "test problem");
+      assert(res.body.detail, "This is a test problem.");
+      done();
+    })
+    .catch((err) => done(err));
 });
 
 test("Get problem list", (done) => {
   request(app)
     .get("/api/problems")
     .expect("Content-Type", /json/)
-    .expect(200, done);
+    .expect(200)
+    .then((res) => {
+      assert(res.body.length, 1);
+      assert(res.body[0].title, "test problem");
+      assert(res.body[0].detail, "This is a test problem.");
+      done();
+    })
+    .catch((err) => done(err));
 });
 
 test("Create new solution", (done) => {
@@ -77,18 +93,63 @@ test("Create new solution", (done) => {
       detail: "This is another test problem.",
     })
     .then((value) => {
+      problem_id_hack = value.body._id;
       request(app)
         .post("/api/solutions")
         .field("problem", value.body._id)
         .attach("file", `${__dirname}/../app.js`)
         .expect("Content-Type", /json/)
-        .expect(200, done);
-    });
+        .expect(201)
+        .then((res) => {
+          solution_id_hack = res.body._id;
+          assert(res.body.problem, value.body._id);
+          assert(res.body.status, "submitted");
+          done();
+        })
+        .catch((err) => done(err));
+    })
+    .catch((err) => done(err));
 });
 
 test("Get solution list", (done) => {
   request(app)
     .get("/api/solutions")
     .expect("Content-Type", /json/)
-    .expect(200, done);
+    .expect(200)
+    .then((res) => {
+      assert(res.body.length, 1);
+      assert(res.body[0].problem.title, "another test problem");
+      assert(res.body[0].status, "submitted");
+      done();
+    })
+    .catch((err) => done(err));
 });
+
+test("Get problem details", (done) => {
+  request(app)
+    .get("/api/problems/" + problem_id_hack) // test problem ID as taken from global variable
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((res) => {
+      assert(res.body._id, problem_id_hack);
+      assert(res.body.title, "test problem");
+      assert(res.body.detail, "This is a test problem");
+      assert(res.body.solution[0]._id, solution_id_hack);
+      done()
+    })
+    .catch((err) => done(err));
+})
+
+test("Get solution details", (done) => {
+  request(app)
+    .get("/api/solutions/" + solution_id_hack) // test solution ID taken from global variable
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((res) => {
+      assert(res.body._id, solution_id_hack);
+      assert(res.body.status, "submitted");
+      assert(res.body.problem._id, problem_id_hack);
+      done()
+    })
+    .catch((err) => done(err))
+})
