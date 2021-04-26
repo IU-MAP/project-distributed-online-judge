825667ea8ff0ae26a5b0f23631ed87423355543d
Trang Nguyen <trangnguyen.281198@gmail.com>
2021-04-26 15:44:11 +0300

    Save downloaded file to directory

--- client/connect.js
+++ client/connect.js
@@ -3,17 +3,30 @@ const fs = require("fs");
 const request = require("request");
 const { exec } = require("child_process");
 
+if (!fs.existsSync(`${__dirname}/data/`)) {
+  fs.mkdirSync(`${__dirname}/data/`);
+}
+
 const host = process.env.DOJ_HOST || "http://localhost:3000";
 const socket = io(host);
 
 socket.on("solution:create", (data) => {
-  const writer = fs.createWriteStream(data._id);
+  const writer = fs.createWriteStream(`data/${data._id}`);
   request(`${host}/uploads/${data._id}`).pipe(writer);
   writer.on("finish", () => {
-    exec(`cat ${data._id}`, (err, stdout, stderr) => {
+    exec(`cat data/${data._id}`, (err, stdout, stderr) => {
       if (err) {
         console.error(err);
       } else {
+        console.log(`Solution ${data._id} content:`);
+        console.log(stdout);
+      }
+    });
+    exec(`node data/${data._id}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Solution ${data._id} output:`);
         console.log(stdout);
       }
     });
