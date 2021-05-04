1af83f21f1f706f4ce97bbb709062c0306a8907c
Trang Nguyen <trangnguyen.281198@gmail.com>
2021-05-05 00:31:50 +0300

    Update solution status when running checker

--- client/connect.js
+++ client/connect.js
@@ -2,6 +2,7 @@ const io = require("socket.io-client");
 const fs = require("fs");
 const request = require("request");
 const { exec } = require("child_process");
+const axios = require("axios");
 
 if (!fs.existsSync(`${__dirname}/data/`)) {
   fs.mkdirSync(`${__dirname}/data/`);
@@ -17,16 +18,37 @@ const socket = io(host);
 socket.on("solution:create", (data) => {
   const writer = fs.createWriteStream(`data/${data._id}`);
   request(`${host}/uploads/${data._id}`).pipe(writer);
-  writer.on("finish", () => {
+  writer.on("finish", async () => {
     //PUT requst for changing status to "running"
-    exec(`./checker.sh ${data._id} ${data.problem} ${host}/uploads`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-      } else {
-        //PUT requst for changing status to failed or OK
-        //stdout will echo AC if all testcases are correct, otherwise WA verdict
-        console.log(stdout);
-      }
+    await axios.put(`${host}/api/solutions/${data._id}`, {
+      problem: data.problem,
+      status: "running",
     });
+    exec(
+      `./checker.sh ${data._id} ${data.problem} ${host}/uploads`,
+      async (err, stdout, stderr) => {
+        if (err) {
+          await axios.put(`${host}/api/solutions/${data._id}`, {
+            problem: data.problem,
+            status: "submitted",
+          });
+        } else {
+          //PUT requst for changing status to failed or OK
+          //stdout will echo AC if all testcases are correct, otherwise WA verdict
+          const outputs = stdout.split("\n").filter((l) => !!l.trim());
+          if (outputs[outputs.length - 1] == "AC") {
+            await axios.put(`${host}/api/solutions/${data._id}`, {
+              problem: data.problem,
+              status: "ok",
+            });
+          } else {
+            await axios.put(`${host}/api/solutions/${data._id}`, {
+              problem: data.problem,
+              status: "failed",
+            });
+          }
+        }
+      }
+    );
   });
 });
